5bd1f5052486b49a9c9c1a81874eec954393e46e
SSenshi147 <47696835+SSenshi147@users.noreply.github.com>
2022-11-22 08:48:17 +0100

    Add project files.

---
+++ Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Extensions;
+public static class IEnumerableExtensions
+{
+    public static bool IsNullOrEmpty<T>(this IEnumerable<T> values)
+    {
+        return values == null || !values.Any();
+    }
+}
---
+++ Extensions/ObjectExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Extensions;
+public static class ObjectExtensions
+{
+    public static void CopyPropertiesFrom(this object self, object source)
+    {
+        var fromProperties = source.GetType().GetProperties();
+        var toProperties = self.GetType().GetProperties();
+
+        foreach (var fromProperty in fromProperties)
+        {
+            foreach (var toProperty in toProperties)
+            {
+                if (fromProperty.Name == toProperty.Name &&
+                    fromProperty.PropertyType == toProperty.PropertyType &&
+                    toProperty.CanWrite)
+                {
+                    toProperty.SetValue(self, fromProperty.GetValue(source));
+                    break;
+                }
+            }
+        }
+    }
+}
