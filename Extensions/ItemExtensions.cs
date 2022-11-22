bbb2382d22a0b680d286388979d9f5f0c23695c7
SSenshi147 <47696835+SSenshi147@users.noreply.github.com>
2022-11-22 17:48:50 +0100

    added more extensions

---
+++ Extensions/ItemExtensions.cs
@@ -0,0 +1,66 @@
+﻿namespace Extensions;
+public static class ItemExtensions
+{
+    public static void CopyPropertiesFrom<T>(this T item, T source)
+    {
+        if (item == null) { throw new ArgumentNullException(nameof(item)); }
+        if (source == null) { throw new ArgumentNullException(nameof(source)); }
+
+        var fromProperties = source.GetType().GetProperties();
+        var toProperties = item.GetType().GetProperties();
+
+        foreach (var fromProperty in fromProperties)
+        {
+            foreach (var toProperty in toProperties)
+            {
+                if (fromProperty.Name == toProperty.Name &&
+                    fromProperty.PropertyType == toProperty.PropertyType &&
+                    toProperty.CanWrite)
+                {
+                    toProperty.SetValue(item, fromProperty.GetValue(source));
+                    break;
+                }
+            }
+        }
+    }
+
+    public static List<T> ToList<T>(this T item)
+    {
+        return new List<T>() { item };
+    }
+
+    public static string ToJson<T>(this T item)
+    {
+        return System.Text.Json.JsonSerializer.Serialize(item);
+    }
+
+    public static string ToJson<T>(this T item,
+                                   System.Text.Json.JsonSerializerOptions jsonSerializerOptions)
+    {
+        return System.Text.Json.JsonSerializer.Serialize(item, jsonSerializerOptions);
+    }
+
+    public static string ToNewtonsoftJson<T>(this T item)
+    {
+        return Newtonsoft.Json.JsonConvert.SerializeObject(item);
+    }
+
+    public static string ToNewtonsoftJson<T>(this T item,
+                                             Newtonsoft.Json.Formatting formatting)
+    {
+        return Newtonsoft.Json.JsonConvert.SerializeObject(item, formatting);
+    }
+
+    public static string ToNewtonsoftJson<T>(this T item,
+                                             Newtonsoft.Json.JsonSerializerSettings jsonSerializerSettings)
+    {
+        return Newtonsoft.Json.JsonConvert.SerializeObject(item, jsonSerializerSettings);
+    }
+
+    public static string ToNewtonsoftJson<T>(this T item,
+                                             Newtonsoft.Json.Formatting formatting,
+                                             Newtonsoft.Json.JsonSerializerSettings jsonSerializerSettings)
+    {
+        return Newtonsoft.Json.JsonConvert.SerializeObject(item, formatting, jsonSerializerSettings);
+    }
+}
