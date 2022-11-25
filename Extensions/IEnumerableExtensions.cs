df56ee93cfc94ad92a65ca39ba3616a7c9214698
SSenshi147 <47696835+SSenshi147@users.noreply.github.com>
2022-11-25 19:34:34 +0100

    singleorthrow

--- Extensions/IEnumerableExtensions.cs
+++ Extensions/IEnumerableExtensions.cs
@@ -5,4 +5,13 @@ public static class IEnumerableExtensions
     {
         return values == null || !values.Any();
     }
+
+    public static T SingleOrThrow<T>(this IEnumerable<T> items, Func<T, bool> selector)
+    {
+        var single = items.SingleOrDefault(selector);
+
+        if (single == null) { throw new Exception("not found"); }
+
+        return single;
+    }
 }
